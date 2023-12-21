ab66ecba1a2b3076148d020d9f6b4e34e4978c5e
FS-Mehmet <140525340+FS-MEHMET@users.noreply.github.com>
2023-12-22 01:49:43 +0300

    last

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "FS-MEHMET"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "awsfsmehmet.click"
+}
